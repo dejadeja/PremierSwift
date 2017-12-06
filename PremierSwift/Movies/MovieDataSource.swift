4f6852285c4bf31c3d86a519a8740dbe68535926
dxjv <dejasces@gmail.com>
2017-12-06 12:27:11 +0000

    Reading from cache to reuse images.

--- PremierSwift/Movies/MovieDataSource.swift
+++ PremierSwift/Movies/MovieDataSource.swift
@@ -12,7 +12,7 @@ import UIKit
 class MovieDataSource {
     //MARK: - Properties
     private var movies: [Movie] = []
-    private let cache = NSCache<NSString, UIImage>()
+    private let cache = NSCache<NSNumber, UIImage>()
 
     //MARK: - Initialiser
     init(movies: [Movie]) {
@@ -35,7 +35,7 @@ extension MovieDataSource {
     }
     
     public func movieThumbnailImage(atIndex index: Int, completion: @escaping APIService.MovieImageCompletionType) {
-        guard let cachedImage = cache.object(forKey: "cachedThumbnail") else {
+        guard let cachedImage = cache.object(forKey: NSNumber(value: index)) else {
             requestImage(atIndex: index, completion: { image in
                 completion(image)
             })
@@ -48,13 +48,14 @@ extension MovieDataSource {
     private func requestImage(atIndex index: Int, completion: @escaping APIService.MovieImageCompletionType) {
         let endpoint = movieThumbnailImageURL(atIndex: index)
 
-        DispatchQueue.global(qos: .background).async { () -> Void in
+        DispatchQueue.global(qos: .background).async { [weak self] () -> Void in
             APIService.retrieveMoviePosterImages(thumbnailURL: endpoint!) { image in
-                guard image != nil else {
+                guard let requestedImage = image else {
                     return
                 }
                 
                 DispatchQueue.main.async {
+                    self?.cache.setObject(requestedImage, forKey: NSNumber(value: index))
                     completion(image)
                 }
             }

732f5d628e862f713dbdf94e535a9f09a76f1425
dxjv <dejasces@gmail.com>
2017-12-07 00:47:56 +0000

    Model creation test
    Tested intialisation of a model object.

---
+++ PremierSwiftTests/Movies/MovieModelTest.swift
@@ -0,0 +1,33 @@
+//
+//  MovieModelTest.swift
+//  PremierSwiftTests
+//
+//  Created by Deja Cespedes on 07/12/2017.
+//  Copyright © 2017 Deliveroo. All rights reserved.
+//
+
+import XCTest
+@testable import PremierSwift
+
+class MovieModelTest: XCTestCase {
+    //MARK: - Properties
+    public var movie: Movie?
+
+    //MARK: - Set up
+    override func setUp() {
+        super.setUp()
+        testCreationOfMovie()
+    }
+    
+    //MARK: - Tear down
+    override func tearDown() {
+        super.tearDown()
+        movie = nil
+    }
+    
+    //MARK: - Test Creation of Movie
+    private func testCreationOfMovie() {
+        let movie = Movie(title: "The Dark Knight", overview: "This is a great movie, I really enjoyed this film. The best in the Batman trilogy", posterPathEndpoint: "\testEndpoint\0")
+        XCTAssertTrue(movie.title != "", "Movie creation complete")
+    }
+}
